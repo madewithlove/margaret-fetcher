c25917ff96948ffa7dbf389b3e50f3a94d54daaa
Maxime Fabre <ehtnam6@gmail.com>
2016-03-20 19:03:57 +0100

    Use MWL config

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,5 @@
-var webpack = require('webpack')
+var config = require('madewithlove-webpack-config').packages;
 
-var env = process.env.BABEL_ENV
-var config = {
-    entry: ['./src'],
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loaders: ['babel-loader'],
-                exclude: /node_modules/,
-            }
-        ]
-    },
-    output: {
-        path: 'dist',
-        filename: 'index.js',
-        library: 'MargaretFetcher',
-        libraryTarget: 'umd'
-    },
-    plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify(env)
-        })
-    ]
-};
-
-if (env === 'production') {
-    config.plugins.push(
-        new webpack.optimize.UglifyJsPlugin({
-            compressor: {
-                pure_getters: true,
-                unsafe: true,
-                unsafe_comps: true,
-                screw_ie8: true,
-                warnings: false
-            }
-        })
-    )
-}
-
-module.exports = config
+module.exports = config({
+    libraryName: 'MargaretFetcher',
+});
