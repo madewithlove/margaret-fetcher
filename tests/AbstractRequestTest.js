2f51dbed56f2e32cca7ed435ccd701dad089a8d3
Maxime Fabre <ehtnam6@gmail.com>
2016-06-10 15:03:29 +0200

    Add test for overlapping requests

--- tests/AbstractRequestTest.js
+++ tests/AbstractRequestTest.js
@@ -173,5 +173,15 @@ describe('AbstractRequest', () => {
                     assert.equal(response.data.options.headers.Authorization, 'Bearer qux');
                 });
         });
+
+        it.skip('can have requests that dont affect each others', () => {
+            return requests.withBearerToken('Foo').make('options').then(response => {
+                assert.equal(response.data.options.headers.Authorization, 'Bearer Foo');
+
+                return requests.make('options').then(response => {
+                    assert.notEqual(response.data.options.headers.Authorization, 'Bearer Foo');
+                });
+            });
+        });
     });
 });
