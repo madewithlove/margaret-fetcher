235a8e9e411e18f4e127ef2f8cb832b4cbf06be6
Maxime Fabre <ehtnam6@gmail.com>
2016-06-17 16:54:59 +0200

    Remove eslint comment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/* eslint object-curly-spacing: 0 */
 export {default as AbstractRequest} from './AbstractRequest';
 export {default as JsonRequest} from './JsonRequest';
 export {default as CrudRequest} from './CrudRequest';
